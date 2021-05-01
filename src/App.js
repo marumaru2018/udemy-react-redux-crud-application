ea4ec4e6961400d068b04c8358e95dee0a63e5a2
marumaru2018 <45307472+marumaru2018@users.noreply.github.com>
2021-05-01 12:26:38 +0900

    learn state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './App.css';
+
+class Counter extends Component {
+  constructor(props){
+    super(props)
+    this.state = {count:0}
+  }
+
+  handlePlusButton = () =>{
+    console.log("handlePlusButton="+this.state.count)
+    this.setState({count: this.state.count + 1})
+  }
+  handleMinusButton = () =>{
+    console.log("handleMinusButton="+this.state.count)
+    this.setState({count: this.state.count - 1})
+  }
+
+  render(){
+    return(
+      <React.Fragment>
+      <div>Counter: {this.state.count}</div>
+      <button onClick={this.handlePlusButton}>+1</button>
+      <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+
+    )
+  }
+}
 
 //function App() {
 const App = () => {
@@ -29,6 +56,7 @@ const App = () => {
 
             ) 
           }
+          <Counter></Counter>
           </React.Fragment>
   );
 }
