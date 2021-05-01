e63e52f8e3b525a9dd9cddcc960ba9d92740642d
marumaru2018 <45307472+marumaru2018@users.noreply.github.com>
2021-05-01 10:47:50 +0900

    learn component

--- src/App.js
+++ src/App.js
@@ -4,18 +4,36 @@ import './App.css';
 
 //function App() {
 const App = () => {
-const greeting = "Hi";
+  const greeting = "Hi";
+  const profiles = [
+    {name:"ichiro",age:10},
+    {name:"jiro",age:20},
+    {name:"saburo",age:30},
+    {name:"shiro"}
+  ];
+  /*
+  <User name={"ichiro"} age={"10"} />
+  <User name={"jiro"} age={"20"} />
+  <User name={"saburo"} age={"30"} />
+  */
+
   return (
           <React.Fragment>
           <h1 className="foo">{greeting}</h1>
           <label htmlFor="bar">bar</label>
           <input type="text" onClick={() => {console.log("clicked")}} />
-          <Cat />
-          <Cat />
-          <Cat />
+          {
+            profiles.map((profile, index) => {
+              return <User name={profile.name} age={profile.age} key={index} />
+            }
+
+            ) 
+          }
           </React.Fragment>
   );
 }
+
+/*
 const Cat = () => {
   return (
     <div>
@@ -23,5 +41,16 @@ const Cat = () => {
     </div>
   );
 }
+*/
+const User = (props) => {
+  return (
+    <div>
+      I am {props.name}, and  {props.age} year old.
+    </div>
+  );
+}
 
+User.defaultProps = {
+  age:99
+};
 export default App;
