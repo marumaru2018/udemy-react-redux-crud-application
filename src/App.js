b5e401707660272a4fbcb0d887cc6aa32f99cec5
marumaru2018 <45307472+marumaru2018@users.noreply.github.com>
2021-05-01 10:17:49 +0900

    change function

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const greeting = "Hi";
+//function App() {
+const App = () => {
+const greeting = "Hi";
   return (
           <React.Fragment>
           <h1 className="foo">{greeting}</h1>
           <label htmlFor="bar">bar</label>
           <input type="text" onClick={() => {console.log("clicked")}} />
+          <Cat />
+          <Cat />
+          <Cat />
           </React.Fragment>
+  );
+}
+const Cat = () => {
+  return (
+    <div>
+      Meow
+    </div>
   );
 }
 
